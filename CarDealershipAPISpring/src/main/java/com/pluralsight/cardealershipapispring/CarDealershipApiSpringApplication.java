ba33370db2188952887f539ad85121a8ec57d549
Ezra A Williams <ewilliamsdaniel@gmail.com>
2024-06-20 08:48:26 -0400

    First commit

---
+++ CarDealershipAPISpring/src/main/java/com/pluralsight/cardealershipapispring/CarDealershipApiSpringApplication.java
@@ -0,0 +1,13 @@
+package com.pluralsight.cardealershipapispring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarDealershipApiSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarDealershipApiSpringApplication.class, args);
+    }
+
+}
---
+++ CarDealershipAPISpring/src/test/java/com/pluralsight/cardealershipapispring/CarDealershipApiSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pluralsight.cardealershipapispring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarDealershipApiSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
